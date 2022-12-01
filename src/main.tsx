aade9ca2df1d0e773cff00427e06ce8c332e791e
Luks3110 <lucas.pereira@arboimoveis.com>
2022-12-01 15:52:24 -0300

    feat: font awesome and project configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider } from "react-router-dom"
+import Router from "./router/Router"
+import "./assets/styles/global.scss"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient: QueryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={Router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
